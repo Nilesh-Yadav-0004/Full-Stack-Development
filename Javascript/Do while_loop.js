7cfa41f98fa370c6fa182fae674aec0a8f618f92
Nilesh-yadav-0004 <yadavnilesh0043@gmail.com>
2025-08-08 15:43:08 +0530

    Tasks

---
+++ Javascript/Do while_loop.js
@@ -0,0 +1,33 @@
+/* DO While Loops  */
+
+let count = 0;
+do{
+    count++
+} while(count<10)
+
+
+  /* Tasks for Do While */
+// Counting Basics.
+
+// Q1. Write a program that uses a do...while loop to print numbers from 1 to 10.
+
+let Count = 1;
+do{
+    count++
+} while(Count < 10)
+
+//  User Input Validation
+
+// Q2. keep asking the user to enter a number between 1 and 5 until they enter a valid number
+
+let num = Process.argv[2];
+console.log(typeof number);
+do{
+    console.log("please the number");
+} while(number=== 1 || number=== 5)
+
+    // Sum Until Zero.
+    // Q3. Continuously ask the user to enter numbers and keep a running total until the user enters 0?
+    
+
+
--- Javascript/while_loop.js
+++ Javascript/while_loop.js
@@ -22,7 +22,7 @@ while (num <= 100){
 
 // Q2. Print even numbers between 1 to 50 using a while loop?
 
-let num =2;
+let nm =2;
 
 while (num <=50){
     console.log(num);
@@ -31,7 +31,7 @@ while (num <=50){
 
 // Q3. Print numbers in reverse from 10 to 1?
 
-let num =10;
+let n =10;
 
 while (num >=1){
     console.log(num);
@@ -40,10 +40,32 @@ while (num >=1){
 
 // Q4. Sum the numbers from 1 to N using a while loop?
 let N =10;
-let num =1;
-let num =0;
+let n1 =1;
+let n2m =0;
 
 while (num <=N){
-    
+    console.log(num);
 }
 
+// Q5. Count how many digits are in a given number using a while loop?
+
+let number = 10;
+let count = 0;
+
+while (num > 0){
+    console.log("Total digits:");
+}
+
+// Q6. Reverse a given number using a while loop?
+
+let = 20;
+
+while (n >=2){
+    console.log(num);
+}
+
+//  Q7. 
+
+
+
+ 
